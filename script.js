e98d0906ff681bcc4e8cf1b94d507852b8cf4fc1
rajsunmukhani <rajsunmukhani123@gmail.com>
2024-04-13 13:35:31 +0530

    made navigation bar with animation

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-var nav = document.querySelector('nav');
-nav.addEventListener("mouseenter",function(){
-    var tl = gsap.timeline()
-    tl.to('.nav-body-overlay',{
-        height : '220%',
-        duration : 0.05
+function navAnim(){
+    var nav = document.querySelector('nav')
+
+    nav.addEventListener('mouseenter',function(){
+        let tl = gsap.timeline();
+
+        tl.to(".nav-bottom",{
+            height : '26vh'
+        })
+        tl.to(".nav-pt2-pane h5",{
+            display : 'block'
+        })
+        tl.to(".nav-pt2-pane h5 span",{
+            delay : -0.7,
+            y : 0,
+            stagger : {
+                amount : 0.3
+            }
+        })
     })
-    tl.from('nav-p2 span',{
-        y : 25
+    nav.addEventListener('mouseleave',function(){
+        let tl = gsap.timeline();
+        
+        tl.to(".nav-pt2-pane h5 span",{
+            // delay : -0.7,
+            y : 25,
+            stagger : {
+                amount : 0.3
+            }
+        })
+        tl.to(".nav-pt2-pane h5",{
+            display : 'none',
+            duration : 0.1
+        })
+        tl.to(".nav-bottom",{
+            delay : -0.5,
+            duration : 0.2,
+            height : 0
+        })
     })
-})
+}
+
