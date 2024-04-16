ad4df4585a7694ffb6844efae4c9e4d7f0c345d4
rajsunmukhani <rajsunmukhani123@gmail.com>
2024-04-16 13:56:12 +0530

    second page in action

--- script.js
+++ script.js
@@ -42,3 +42,29 @@ function navAnim(){
 
 navAnim();
 
+function page2Anim(){
+    var pane = document.querySelectorAll(".page2-right .pg2-right-pt")
+    
+    pane.forEach(function(elem){
+        elem.addEventListener("mouseenter",function(){
+            gsap.to(elem.childNodes[3],{
+                opacity : 1,
+                scale : 1
+            })
+        })
+        elem.addEventListener("mouseleave",function(){
+            gsap.to(elem.childNodes[3],{
+                opacity : 0,
+                scale : 0.5
+            })
+        })
+        elem.addEventListener("mousemove",function(dets){
+            gsap.to(elem.childNodes[3],{
+                x : dets.x - elem.getBoundingClientRect().x - 50 ,
+                y : dets.y - elem.getBoundingClientRect().y - 150 
+            })
+        })
+    })
+}
+
+page2Anim();
